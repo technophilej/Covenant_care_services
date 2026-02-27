627f06b40b38afa7a7197fc350c75c2117cd6fc9
technophilej <64825030+technophilej@users.noreply.github.com>
2026-02-27 00:20:16 -0500

    chore: add eslint config and local logo asset

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ['node_modules/', '.next/', 'out/', 'dist/'],
+};
