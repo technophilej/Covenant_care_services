741e809f0264ddcafedbf5bbf43b46e1aefa4b1d
technophilej <64825030+technophilej@users.noreply.github.com>
2026-02-26 19:21:05 -0500

    Reapply "Limit site to three pages and match navigation bar layout"
    This reverts commit e6814f541832eea309d094b150f164751af7a07a.

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,16 @@
 import HeroSection from '../components/home/HeroSection'
-import IntroductionSection from '../components/home/IntroductionSection'
-import PcaServicesOverview from '../components/home/PcaServicesOverview'
-import HowItWorksSection from '../components/home/HowItWorksSection'
-import ComplianceSafetySection from '../components/home/ComplianceSafetySection'
-import OurStaffSection from '../components/home/OurStaffSection'
-import WireframeCTASection from '../components/home/WireframeCTASection'
+import ServicesPreview from '../components/home/ServicesPreview'
+import AboutSection from '../components/home/AboutSection'
+import CTASection from '../components/home/CTASection'
 import Layout from '../components/layout/Layout'
 
 export default function HomePage() {
   return (
     <Layout>
       <HeroSection />
-      <IntroductionSection />
-      <PcaServicesOverview />
-      <HowItWorksSection />
-      <ComplianceSafetySection />
-      <OurStaffSection />
-      <WireframeCTASection />
+      <ServicesPreview />
+      <AboutSection />
+      <CTASection />
     </Layout>
   )
 }
